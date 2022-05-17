c21cd31f04a5254beee3b5932290f6546f18374c
louiejay00 <louie.jay.becudo.99@gmail.com>
2022-05-17 22:46:15 +0800

    master

--- frontend/src/pages/configurations.js
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@material-ui/core";
-
-export default function ConfigurationsPage() {
-  return (
-    <div>
-      <Typography>Configurations Page</Typography>
-    </div>
-  );
-}
